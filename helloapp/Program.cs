130c57f9dd7ed654c0c2a4903b8551eec21bf415
Danil_Titarenko <danil.titarenko@gmail.com>
2021-11-03 22:50:25 +0500

    Add GreetBlack()

--- helloapp/Program.cs
+++ helloapp/Program.cs
@@ -4,6 +4,10 @@ namespace helloapp
 {
     class Program
     {
+        public static void GreetBlack()
+        {
+            Console.WriteLine("hello, black");
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Hello everyone");
