56443251052f328082bc7c5592e0c083c4d83a88
Danil_Titarenko <danil.titarenko@gmail.com>
2021-11-03 23:18:30 +0500

    Black is working

--- helloapp/Program.cs
+++ helloapp/Program.cs
@@ -4,19 +4,21 @@ namespace helloapp
 {
     class Program
     {
-         
+
         public static void GreetBlack()
         {
             Console.WriteLine("hello black");
+        }
 
         public static void GreetWhite()
         {
             Console.WriteLine("hello white");
-        main
         }
         static void Main(string[] args)
         {
             Console.WriteLine("Hello everyone");
+            GreetBlack();
+            GreetWhite();
         }
     }
 }
