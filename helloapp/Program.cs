4da2683a4c7cc75a5af14a823e3334602fbd57c7
Danil_Titarenko <danil.titarenko@gmail.com>
2021-11-03 22:28:43 +0500

    Add Hello everyone

--- helloapp/Program.cs
+++ helloapp/Program.cs
@@ -6,7 +6,7 @@ namespace helloapp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello everyone");
         }
     }
 }
